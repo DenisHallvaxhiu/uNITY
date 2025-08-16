f54e8737be81fc30dcadcc362e5e39631a7fb7f6
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-08-16 09:08:47 -0400

    Ep3/4
    Added player movement and camera movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,10 +5,18 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
 
+    public float forwardForce = 300f;
+    public float sidewaysForce = 500f;
 
-
-    // Update is called once per frame
+    // For physics use FixedUpdate instead of Update
     void FixedUpdate() {
-        rb.AddForce(0,0,2000 * Time.deltaTime);
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0);
+        }
     }
 }
