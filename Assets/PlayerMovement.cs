c94918cc2fd2ffaf3bbe7107d2e46c04ec7694be
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-08-16 08:30:23 -0400

    Ep2
    Added script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Rigidbody rb;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb.AddForce(0,0,2000 * Time.deltaTime);
+    }
+}
