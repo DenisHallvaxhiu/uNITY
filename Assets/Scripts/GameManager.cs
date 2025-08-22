29227929b4743cb1f5b1eca1bad0496c6827913b
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-08-22 09:03:41 -0400

    Ep 9
    Animation/ Next Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     bool gameHasEnded = false;
+
     public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+
     public void EndGame() {
         if(gameHasEnded == false) {
             gameHasEnded = true;
@@ -17,5 +22,9 @@ public class GameManager : MonoBehaviour {
 
     void Restart() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
     }
 }
---
+++ Assets/Scripts/endTrigger.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class endTrigger : MonoBehaviour {
+    public GameManager gameManager;
+    void OnTriggerEnter() {
+        gameManager.CompleteLevel();
+    }
+}
